013b581f3a4e6b76727704345c975e91024889f7
Nick Barrett <nick@oxygem.com>
2015-10-18 17:38:42 +0100

    Misc tidyup/bits.

--- boot.py
+++ boot.py
@@ -1,6 +1,7 @@
+# flake8: noqa
 # Source Server Stats
 # File: boot.py
 # Desc: bootstrap/import the API
 
-from sourcestats.app import app # noqa
-from sourcestats.views.api import servers, maps, games # noqa
+from sourcestats.app import app
+from sourcestats.views.api import servers, maps, games, history
