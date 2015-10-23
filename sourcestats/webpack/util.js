1f18956589b16ffc6cc312d104d113139b516e6d
Nick Barrett <nick@oxygem.com>
2015-10-23 21:17:15 +0100

    Support user timezones (and discover JS date fails).

--- sourcestats/webpack/util.js
+++ sourcestats/webpack/util.js
@@ -2,10 +2,33 @@
 // File: sourcestats/webpack/util.js
 // Desc: utility functions for the frontend
 
+import _ from 'lodash';
+
+
+export function parseDates(list, accessor) {
+    const dates = [];
+
+    _.each(list, value => {
+        dates.push({
+            date: new Date(value.datetime),
+            value: value[accessor]
+        });
+    });
+
+    return dates;
+}
+
 
 export function timeAgo(dateString) {
-    const date = new Date(dateString);
-    const seconds = Math.floor((new Date() - date) / 1000);
+    // In UTC time
+    const then = new Date(dateString).getTime();
+
+    // In user local time
+    let now = new Date();
+
+    // Add the offset (JavaScript date handling is a joke)
+    now = now.getTime() + (now.getTimezoneOffset() * 60000);
+    const seconds = Math.floor((now - then) / 1000);
 
     let interval = Math.floor(seconds / 31536000);
 
