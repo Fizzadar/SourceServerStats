04bf8ce8bf90a9a3535d4583bf8e04157a250a59
Nick Barrett <nick@oxygem.com>
2015-11-14 18:22:56 +0000

    Force rounded values to be ints.

--- sourcestats/util/elastic.py
+++ sourcestats/util/elastic.py
@@ -198,7 +198,9 @@ def get_es_history(
         }
 
         for field in fields:
-            date_bucket[field] = round(bucket[field]['value'] / interval_multipliter)
+            date_bucket[field] = int(round(
+                bucket[field]['value'] / interval_multipliter
+            ))
 
         date_histogram.append(date_bucket)
 
